b0c9e9a00fc569001e684008011a2ebdeecac179
wangying <13571877753@163.com>
2021-10-23 15:06:13 +0800

    修改提交

--- autotest_ui/autotest_ui/testrunner.py
+++ autotest_ui/autotest_ui/testrunner.py
@@ -5,6 +5,6 @@ import os
 report = setting.REPORT_PATH
 temp = setting.TEMP_PATH
 pytest.main(['-s','-v','--reruns=0','--alluredir',f'{temp}','--allure-epics=test3'])
-os.system(f'/Users/wangying/allure-2.13.1/bin/allure generate {temp} -o {report} --clean')
+os.system(f'allure generate {temp} -o {report} --clean')
 
 
